247d4cfb6af65f6b6cf96e6194724ab074c18828
Ravivarman16 <129171351+Ravivarman16@users.noreply.github.com>
2023-09-17 12:36:24 +0530

    script to install k8s

--- prod.sh
+++ prod.sh
@@ -24,6 +24,3 @@ apt-get update
 apt-get install -y kubelet kubeadm kubectl
 systemctl enable kubelet.service
 systemctl start kubelet
-
-
-kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
